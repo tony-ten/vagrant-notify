02bb8c3f331c2b9d7afbf361763cbbc51e204688
Tony Baltazar <abaltazar@grindnetworks.com>
2015-10-21 17:13:05 -0700

    Adding start/stop/restart options to command

--- lib/vagrant-notify/command.rb
+++ lib/vagrant-notify/command.rb
@@ -5,33 +5,60 @@ module Vagrant
   module Notify
     class Command < Vagrant.plugin('2', :command)
 
-      # Show description when `vagrant list-commands` is triggered
       def self.synopsis
         "plugin: vagrant-notify: forwards notify-send from guest to host machine and notifies provisioning status"
       end
 
       def execute
-        options = {}
+      options = {}
         opts = OptionParser.new do |o|
-          o.banner = 'Usage: vagrant notify --status'
+          o.banner = 'Usage: vagrant notify'
           o.separator ''
           o.version = Vagrant::Notify::VERSION
           o.program_name = 'vagrant notify'
 
-          o.on('--status', String, 'Show vagrant-notify-server daemon status. (default)')
+          o.on('-h', '--help', 'Display this screen.' )                           { options[:help] = true }
+          o.on('--status', 'Show vagrant-notify-server daemon status. (default)') { options[:status] = true }
+          o.on('--start', 'Manually start vagrant-notify-server daemon.')         { options[:start] = true }
+          o.on('--stop', 'Manually stop vagrant-notify-server daemon.')           { options[:stop] = true }
+          o.on('--restart', 'Manually restart vagrant-notify-server daemon.')     { options[:restart] = true }
         end
 
         argv = parse_options(opts)
-        options[:provider] ||= @env.default_provider
-
+       
         with_target_vms(argv, options) do |machine|
-          unless argv.nil?
+          if options[:help]
+            @env.ui.info(opts)
+            return 0
+          end
+          if options[:status] || options.length == 0
             @env.action_runner.run(Vagrant::Notify::Action.action_status_server, {
               :machine => machine,
             })
+            return 0
+          end
+          if options[:stop]
+            @env.action_runner.run(Vagrant::Notify::Action.action_stop_server, {
+              :machine => machine,
+            })
+            return 0
+          end
+          if options[:start]
+            @env.action_runner.run(Vagrant::Notify::Action.action_start_server, {
+              :machine => machine,
+            })
+            return 0
+          end
+          if options[:restart]
+            @env.action_runner.run(Vagrant::Notify::Action.action_stop_server, {
+              :machine => machine,
+            })
+            @env.action_runner.run(Vagrant::Notify::Action.action_start_server, {
+              :machine => machine,
+            })
+            return 0
           end
         end
-
       end
 
     end
